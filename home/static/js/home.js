04f7e188cfc4e460188e3f893f74e07e09082771
João Vitor <joao_vitor_silvestre@outlook.com>
2016-04-27 15:22:12 -0300

    Home css done

---
+++ home/static/js/home.js
@@ -0,0 +1,9 @@
+function login_over(on) {
+    var login_button = document.getElementById("login_button")
+    if (on == true){
+        login_button.style.backgroundColor = "#439AAB"
+    }
+    else{
+        login_button.style.backgroundColor = "#204A52"
+    }
+}
